d7de794b75bf919b16939a4d710dc2c47353867e
I461268 <d.picus@student.fontys.nl>
2021-03-23 10:13:58 +0100

    initial commit

---
+++ MenuApp.MenuService.EntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MenuApp.MenuService.EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
