02a23f2cdd5d4c321cf321451de79a9b461cea96
Tycho van Isterdael <t.vanisterdael@gmail.com>
2021-05-16 13:14:23 +0200

    Added descriptions

--- MenuApp.MenuService.Logic/Entities/MenuItem.cs
+++ MenuApp.MenuService.Logic/Entities/MenuItem.cs
@@ -11,5 +11,6 @@ namespace MenuApp.MenuService.Logic.Entities
         public List<Ingredient> Ingredients { get; set; }
         public double Price { get; set; }
         public string Image { get; set; }
+        public string Description { get; set; }
     }
 }
--- MenuApp.MenuService/Controllers/MenuItemController.cs
+++ MenuApp.MenuService/Controllers/MenuItemController.cs
@@ -26,7 +26,8 @@ namespace MenuApp.MenuService.Controllers
                 Type = "Voor",
                 Ingredients = new List<Ingredient> {new Ingredient(), new Ingredient()},
                 Price = 12.15,
-                Image = "https://static.ah.nl/static/product/AHI_43545239363939373134_1_LowRes_JPG.JPG?options=399,q85"
+                Image = "https://static.ah.nl/static/product/AHI_43545239363939373134_1_LowRes_JPG.JPG?options=399,q85",
+                Description = "Fresh from a can! Yum yum."
             };
             MenuItem item2 = new MenuItem
             {
@@ -35,7 +36,8 @@ namespace MenuApp.MenuService.Controllers
                 Type = "Hoofd",
                 Ingredients = new List<Ingredient> {new Ingredient(), new Ingredient()},
                 Price = 67.15,
-                Image = "https://static.ah.nl/static/recepten/img_062491_890x594_JPG.jpg"
+                Image = "https://static.ah.nl/static/recepten/img_062491_890x594_JPG.jpg",
+                Description = "Dead cow, very yummy."
             };
             MenuItem item3 = new MenuItem
             {
@@ -44,7 +46,8 @@ namespace MenuApp.MenuService.Controllers
                 Type = "Na",
                 Ingredients = new List<Ingredient> {new Ingredient(), new Ingredient()},
                 Price = 100.15,
-                Image = "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/77384360-B5BC-4737-805E-C83023F3F281/Derivates/10B65431-A7A2-4182-831E-C0228FFD3EA7.jpg"
+                Image = "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/77384360-B5BC-4737-805E-C83023F3F281/Derivates/10B65431-A7A2-4182-831E-C0228FFD3EA7.jpg",
+                Description = "Delicious sugar set aflame and caramalised."
             };
 
             List<MenuItem> menu = new List<MenuItem>();
